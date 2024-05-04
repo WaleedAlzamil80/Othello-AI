a216e2f9f620f22e96b1652a0f3e25ffd5e60dc4
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2024-05-04 19:05:53 +0300

    map device

--- training_eps.py
+++ training_eps.py
@@ -28,14 +28,10 @@ parser.add_argument("--data_path", type=str, default="./Othello-AI/RL/DataGenera
 
 args = parser.parse_args()
 
-model = ResNet(args.NB, args.BatchSize)
+model = ResNet(args.NB, args.BatchSize).to(device)
 
 print("Loading pretrained model to ", device)
-if cuda:
-    state_dict = torch.load(args.model_file)
-else:
-    state_dict = torch.load(args.model_file, map_location = torch.device('cpu'))
-
+state_dict = torch.load(args.model_file, map_location = device)
 model.load_state_dict(state_dict)
 
 optimizer = torch.optim.Adam(model.parameters(), lr = args.lr)
