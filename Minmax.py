46b7cc800975b528d503e1baf8c1348acfb83c40
adhammohamed01 <adham.mhamed2001@gmail.com>
2024-05-16 06:44:15 +0300

    commit

--- Minmax.py
+++ Minmax.py
@@ -1,10 +1,12 @@
-
+import time
 import numpy as np
 import copy
 
 class Minmax:
 
-    def get_best_move(board, depth, alpha_beta = False):
+    start_time = 0
+
+    def get_best_move(board, depth, alpha_beta = False, time_constrain = False):
         best_move = None
         max_eval = float('-inf')
         valid_moves = board.get_valid_moves()
@@ -13,9 +15,15 @@ class Minmax:
             print(move)
             board.make_move(*move)
             if alpha_beta:
-                eval = Minmax.minimax_alpha_beta(depth - 1, float('-inf'), float('inf'), False, board)
+                if time_constrain:
+                  eval = Minmax.minimax_alpha_beta_time_constrain(float('-inf'), float('inf'), False, board, 1000)
+
+                else:
+                  eval = Minmax.minimax_alpha_beta(depth - 1, float('-inf'), float('inf'), False, board)
+
             else:
                 eval = Minmax.minimax(depth - 1, False)
+
             board.board = copy.deepcopy(previous_board)
 
             if eval > max_eval:
@@ -23,6 +31,51 @@ class Minmax:
                 best_move = move
 
         return best_move
+
+    def minimax_alpha_beta_time_constrain(depth, alpha, beta, maximizing_player, board):
+        if depth == 1000:
+          Minmax.start_time = time.time()
+
+        if depth == 0:
+            board.current_player *= -1
+            return Minmax.evaluate_heuristic(board)
+
+        valid_moves = board.get_valid_moves()
+        if maximizing_player:
+            max_eval = float('-inf')
+            for move in valid_moves:
+                previous_board = copy.deepcopy(board.board)
+                board.make_move(*move)
+
+                if (time.time() - Minmax.start_time) < 10:
+                  eval = Minmax.minimax_alpha_beta(depth - 1, alpha, beta, False, board)
+                else:
+                  eval = Minmax.minimax_alpha_beta(0, alpha, beta, False, board)
+
+                board.board = previous_board
+                max_eval = max(max_eval, eval)
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    break
+            board.current_player *= -1
+            return max_eval
+
+        else:
+            min_eval = float('inf')
+            for move in valid_moves:
+                previous_board = copy.deepcopy(board.board)
+                board.make_move(*move)
+                if (time.time() - Minmax.start_time) < 10:
+                  eval = Minmax.minimax_alpha_beta(depth - 1, alpha, beta, True, board)
+                else:
+                  eval = Minmax.minimax_alpha_beta(0, alpha, beta, True, board)
+                board.board = copy.deepcopy(previous_board)
+                min_eval = min(min_eval, eval)
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    break
+            board.current_player *= -1
+            return min_eval
 
     def minimax_alpha_beta(depth, alpha, beta, maximizing_player, board):
         valid_moves = board.get_valid_moves()
@@ -100,9 +153,54 @@ class Minmax:
             return 100 * (max_player_mobility - min_player_mobility) / (max_player_mobility + min_player_mobility + 1)
 
         def corners_captured():
-            return 0
+            maxCorners = 0
+            minCorners = 0
+            if board[0][0] == 1:
+                maxCorners += 1
+            elif board[0][0] == -1:
+                minCorners += 1
+            if board[0][7] == 1:
+                maxCorners += 1
+            elif board[0][7] == -1:
+                minCorners += 1
+            if board[7][0] == 1:
+                maxCorners += 1
+            elif board[7][0] == -1:
+                minCorners += 1
+            if board[7][7] == 1:
+                maxCorners += 1
+            elif board[7][7] == -1:
+                minCorners += 1
+            if maxCorners + minCorners != 0:
+                return 100.0 * (maxCorners - minCorners) / (maxCorners + minCorners)
+            else:
+                return 0
 
-        def stability():
-            return 0
+        def stability_value(board, player):
+          weights = {'stable': 1, 'semi-stable': 0, 'unstable': -1}
+          stable_count = 0
+          semi_stable_count = 0
+          unstable_count = 0
 
-        return coin_parity() + mobility() + corners_captured() + stability()
+          for row in range(board.board_size):
+              for col in range(board.board_size):
+                  if board.board[row, col] == player:
+                      if (row == 0 or row == board.board_size - 1) and (col == 0 or col == board.board_size - 1):
+                          stable_count += 1
+                      else:
+                          neighbors = [(row + dr, col + dc) for dr in range(-1, 2) for dc in range(-1, 2)
+                                      if 0 <= row + dr < board.board_size and 0 <= col + dc < board.board_size]
+                          neighbor_values = [board.board[r, c] for r, c in neighbors]
+                          if ' ' in neighbor_values and player in neighbor_values:
+                              semi_stable_count += 1
+                          else:
+                              unstable_count += 1
+
+          return weights['stable'] * stable_count + weights['semi-stable'] * semi_stable_count + weights['unstable'] * unstable_count
+
+        def stability(board):
+            max_player_stability = stability_value(board, board.current_player)
+            min_player_stability = stability_value(board, -board.current_player)
+            return 100 * (max_player_stability - min_player_stability) / (max_player_stability + min_player_stability + 1)
+
+        return coin_parity() + mobility() + corners_captured() + stability(board)
